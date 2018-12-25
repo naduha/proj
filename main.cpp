973f09d9cec1f414d0dee59b16a7f49e6a53d81c
Nadia Stafeeva <ms.stafeeva@mail.ru>
2018-12-25 03:32:30 +0300

    main

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+
+
+
+
+using namespace std;
+
+int main()
+{
+	double a;
+	double b;
+	int count;
+	cout << "Vvedite pervoe chislo: ";
+	cin >> a;
+	cout << "Vvedite vtoroe chislo: ";
+	cin >> b;
+	cout << "1-vuch, 2-del, 3-sum, 4-ymn " << endl;
+	cout << "VVedite deistvie: ";
+	cin >> count;
+	switch (count)
+	{
+	case 1:
+	{
+		cout << "He 7oToBo" << endl;
+		break;
+	}
+	case 2:
+	{
+		cout << "He 7oToBo" << endl;
+		break;
+	}
+	case 3:
+	{
+		cout << "He 7oToBo" << endl ;
+		break;
+	}
+	case 4:
+	{
+		cout << "He 7oToBo" << endl;
+		break;
+	}
+	}
+	system("pause");
+	return 0;
+}
